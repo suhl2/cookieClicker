3160582b4d2de88c99b6004398c8a757cedb3dfd
Sarah <sarah.celiann@gmail.com>
2022-12-16 16:48:04 -0600

    Add ornaments button

--- index.js
+++ index.js
@@ -6,6 +6,13 @@
 //Trackers
 let presents = 0;
 let presentsPerSecond = 0;
+let producers = [
+    {kind: "ornament", cost: 10, increment: 5, presentIncrease: 1},
+    {kind: "garland", cost: 50, increment: 20, presentIncrease: 5},
+    {kind: "candy-cane", cost: 100, increment: 50, presentIncrease: 10},
+    {kind: "star", cost: 5000}
+]
+
 
 //Clicking on tree generates one present
 const tree = document.getElementById("tree");
@@ -15,3 +22,18 @@ const addPresentOnClick = () => {
     presentText.innerText =`Presents: ${presents}`;
 }
 tree.addEventListener("click", addPresentOnClick);
+
+//Buying a producer increments coffee/second
+//start with ornament
+const ornamentsButton = document.getElementById("ornaments");
+const ornamentsOwnedText = document.getElementById("ornaments-owned");
+console.log(ornamentsOwnedText);
+let ornamentsOwned = 0;
+const buyOrnaments = () => {
+    ornamentsOwned++;
+    presentsPerSecond += 1;
+    ornamentsOwnedText.innerText = `Owned: ${ornamentsOwned}`;
+}
+ornamentsButton.addEventListener("click", buyOrnaments);
+
+//increment presents once per second
